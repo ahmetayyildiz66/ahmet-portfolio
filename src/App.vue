b005473c36b74718e2f4aeda5027fe612e00361d
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-02-02 21:55:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="app">
+    TESTIONG
+  </div>
+</template>
+<style lang="scss">
+@import "../style/main.scss";
+.app {
+  background-color: $color-secondary;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
