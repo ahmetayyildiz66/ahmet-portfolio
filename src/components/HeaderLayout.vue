964ac539ce7a41586564b226c1592bc06fdc7de9
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-02-03 20:50:49 +0300

    Home page desktop is completed

---
+++ src/components/HeaderLayout.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <router-link to="/">
+      <LogoIcon class="header__logo" />
+    </router-link>
+    <nav class="navigation">
+      <router-link
+        to="/projects"
+        tag="a"
+        class="navigation__item navigation__item--projects"
+        >Projects</router-link
+      >
+      <router-link
+        to="/"
+        tag="a"
+        class="navigation__item navigation__item--skills"
+        >Skills</router-link
+      >
+    </nav>
+  </header>
+</template>
+
+<script>
+import LogoIcon from "../assets/LogoIcon.svg";
+
+export default {
+  name: "HeaderLayout",
+  components: {
+    LogoIcon
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  color: $color-white;
+  // background-color: orangered;
+  height: $size-header-height;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 2rem;
+}
+
+.navigation {
+  width: 27.5rem;
+  height: 6.1rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    text-align: center;
+    padding: 0 1rem;
+    border-bottom: 3px solid transparent;
+
+    &:hover {
+      border-bottom-color: $color-primary;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="fullname">
+      <span class="fullname__item fullname__firstname">Ahmet</span>
+      <span class="fullname__item fullname__lastname">AYYILDIZ</span>
+    </h1>
+    <h2 class="job">Front-End Developer</h2>
+    <div class="social">
+      <a href="https://github.com/ahmetayyildiz66">
+        <GithubIcon />
+      </a>
+      <a href="https://www.linkedin.com/in/ahmet-ayyildiz-841452138/">
+        <LinkedinIcon />
+      </a>
+      <a href="mailto:ahmetayyildiz66@gmail.com">
+        <MailIcon />
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import GithubIcon from "@/assets/GithubIcon.svg";
+import LinkedinIcon from "@/assets/LinkedinIcon.svg";
+import MailIcon from "@/assets/MailIcon.svg";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    GithubIcon,
+    LinkedinIcon,
+    MailIcon
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  margin-top: 12rem;
+  color: $color-white;
+}
+
+.fullname {
+  &__item {
+    font-weight: 500;
+    font-size: 80px;
+    display: block;
+    letter-spacing: 2rem;
+  }
+}
+
+.job {
+  margin-top: 8.5rem;
+  font-weight: 500;
+  font-size: 3rem;
+  color: $color-primary;
+}
+
+.social {
+  margin-top: 3.5rem;
+  width: 26.8rem;
+  height: 7.2rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
